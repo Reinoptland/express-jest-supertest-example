497c72794a8b44fc75a2c9b33f37879f57ad1187
Reinoptland <reinoptland@gmail.com>
2020-06-09 08:55:50 +0200

    Tests for users/:id

--- routers/users.spec.js
+++ routers/users.spec.js
@@ -7,9 +7,29 @@ describe("User routes", () => {
   describe("GET /users", () => {
     test("should return an array of user objects", async (done) => {
       const res = await request.get("/users");
-      console.log(res.body);
       expect(res.status).toBe(200);
       expect(res.body.length).toEqual(5);
+      done();
+    });
+  });
+
+  describe("GET /users/:id", () => {
+    test("should respond with a specific user", async (done) => {
+      const res = await request.get("/users/1");
+      expect(res.status).toEqual(200);
+      expect(res.body).toEqual({
+        id: 1,
+        firstName: "Irene",
+        lastName: "de Nicolo",
+      });
+
+      done();
+    });
+
+    test("should respond with 404 if the user is not found", async (done) => {
+      const res = await request.get("/users/99");
+      expect(res.status).toEqual(200);
+      expect(res.body.message).toEqual("User no found");
       done();
     });
   });
